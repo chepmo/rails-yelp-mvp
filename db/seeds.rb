6a44c9c1ce16ac1e1c574bcad2d6e9908381190d
Morgane Le Deu <chep.mo@hotmail.fr>
2022-11-16 14:49:19 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create(name: "Gorgio", address: "San Remo", phone_number: "0611111111", category: "italian")
+Restaurant.create(name: "La Frite", address: "Bruxelles", phone_number: "0622222222", category: "belgian")
+Restaurant.create(name: "Sushi", address: "Tokyo", phone_number: "0633333333", category: "japanese")
+Restaurant.create(name: "Grenouille", address: "Nice", phone_number: "0644444444", category: "french")
+Restaurant.create(name: "Nems", address: "Pekin", phone_number: "0655555555", category: "chinese")
